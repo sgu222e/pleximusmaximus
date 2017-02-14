78625dab07eb3234d8ba021564a2fa031ffadedf
sgu222e <leighc@gmail.com>
2017-02-14 14:06:58 -0400

    Add files via upload

--- pleximusmaximus.bat
+++ pleximusmaximus.bat
@@ -1,30 +1,37 @@
 @ECHO OFF
-REM Plex Windows Service Status Check and Restart
+REM Plex Windows Status Check and Restart
 REM 2017 - Leigh Cameron
 REM Use at your own risk, paths and names may need to be updated
 REM GPL v3
 REM Requires curl.exe and libcurl.dll to function -> https://curl.haxx.se/download.html (grab Win32 version and libcurl.dll)
 REM I placed curl and libcurl.dll into the same path as the batch to keep it simple and portable.
 
-SET SERVNAME="Plex Media Server.exe"	REM This is the Plex Service name
-SET PLEXWEB="http://localhost:32400/web/index.html"	REM This is the web url for the local plex web app
+SETLOCAL EnableExtensions
+SET "PMSPATH=X:\Program Files (x86)\Plex\Plex Media Server"		REM Set you servers PMS Path Here!!!
 
-sc query %SERVNAME% %1 | find "RUNNING"
-if %ERRORLEVEL% == 2 goto trouble
-if %ERRORLEVEL% == 1 goto stopped
-if %ERRORLEVEL% == 0 goto started
+REM Do Not Edit Below this line!!! This means you Julius.
+
+SET EXENAME="Plex Media Server.exe"	REM This is the Plex Service name
+SET "PEXE=Plex Media Server.exe"	REM This is for Tasklist to work
+SET "PLEXWEB=http://localhost:32400/web/index.html"	REM This is the web url for the local plex web app
+
+FOR /F "tokens=1 delims=,  " %%x IN ('tasklist /NH /FO CSV /FI "IMAGENAME eq %PEXE%"') DO IF %%x == "%PEXE%" goto started
+
+goto stopped
+
 echo unknown status
 goto end
 :trouble
 echo Oh noooo.. trouble, use manual intervention, or call someone you trust
 goto end
 :started
-echo %SERVNAME% is started, keep calm and carry on
+echo %PEXE% is started, keep calm and carry on
 goto web
 :stopped
-echo $SERVNAME% is stopped, unleash the restart dragons
-echo Starting service
-net start %SERVNAME%
+echo %PEXE% is stopped, unleash the restart dragons
+echo Starting Plex Media Server
+cd /d %PMSPATH%
+%EXENAME%
 goto end
 :erro
 echo Error please check your command.. hopefully you don't get this, seriously 
@@ -41,11 +48,15 @@ echo All is well on the webfront, carry on
 goto end
 :webstop
 echo Web app not responding, must restart Plex Service!
-echo Stopping service
-net stop %SERVNAME%
-ping 127.0.0.1 -n 10 > nul		REM This is effectively a sleep for 10 seconds, adjust as needed
-echo Starting service
-net start %SERVNAME%
+echo Terminating Plex Media Server
+taskkill /F /IM "%PEXE%"
+ping 127.0.0.1 -n 10 > nul		REM This is effectively a sleep for 10 seconds, adjust as needed... or follow the rule above.
+echo Starting Plex Media Server
+cd /d %PMSPATH%
+%EXENAME%
 goto end
 :end
-exit
+REM exit
+
+
+
