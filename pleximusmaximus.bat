6abe7b60244a8ff6988e861f9bec059eebc7c388
sgu222e <leighc@gmail.com>
2017-02-14 16:20:37 -0400

    Add files via upload
    moved url

--- pleximusmaximus.bat
+++ pleximusmaximus.bat
@@ -7,13 +7,14 @@ REM Requires curl.exe and libcurl.dll to function -> https://curl.haxx.se/downlo
 REM I placed curl and libcurl.dll into the same path as the batch to keep it simple and portable.
 
 SETLOCAL EnableExtensions
-SET "PMSPATH=C:\Program Files (x86)\Plex\Plex Media Server"		REM Set you servers PMS Path Here!!!
+
+SET "PMSPATH=E:\Program Files (x86)\Plex\Plex Media Server"		REM Set you servers PMS Path Here!!!
+SET "PLEXWEB=http://127.0.0.1:32400/web/index.html"				REM This is the web url for the local plex web app, it should work as is, but may require an edit.
 
 REM Do Not Edit Below this line!!! This means you Julius.
 
 SET EXENAME="Plex Media Server.exe"	REM This is the Plex Service name
 SET "PEXE=Plex Media Server.exe"	REM This is for Tasklist to work
-SET "PLEXWEB=http://127.0.0.1:32400/web/index.html"	REM This is the web url for the local plex web app
 
 FOR /F "tokens=1 delims=,  " %%x IN ('tasklist /NH /FO CSV /FI "IMAGENAME eq %PEXE%"') DO IF %%x == "%PEXE%" goto started
 
